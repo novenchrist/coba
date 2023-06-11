5ac5fabe9cdf4794766adf0ccab0d8cc4a7d12ad
novenchrist <noven0811@gmail.com>
2023-06-11 19:38:58 +0700

    1st

---
+++ index.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Rocky</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+</head>
+
+<body id="gambar">
+    <h1>Rocky</h1>
+    <div class="container">
+        <ul class="gallery">
+            <li>
+                <a href="#gambar1">
+                    <img src="img/thumbs/1.png" alt="Rocky" class="icon">
+                    <span>1</span>
+                </a>
+                <div class="overlay" id="gambar1">
+                    <div class="bingkai">
+                        <a href="#gambar" class="close">X</a>
+                        <a href="#gambar13" class="prev"></a>
+                        <img src="img/full/1.png" alt="Rocky">
+                        <a href="#gambar3" class="next"></a>
+                        <?php for ($i = 1; $i <= 7; $i += 2) {
+                            echo '<a href="#gambar' . $i . '"class="bullet">' . $i . '</a> ';
+                        } ?>
+                        <a href="#gambar1" class="bullet">1</a>
+                        <a href="#gambar3" class="bullet">2</a>
+                        <a href="#gambar5" class="bullet">3</a>
+                        <a href="#gambar7" class="bullet">4</a>
+                        <a href="#gambar8" class="bullet">5</a>
+                        <a href="#gambar9" class="bullet">6</a>
+                        <a href="#gambar10" class="bullet">7</a>
+                        <a href="#gambar11" class="bullet">8</a>
+                        <a href="#gambar12" class="bullet">9</a>
+                        <a href="#gambar13" class="bullet">10</a>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <a href="#gambar3">
+                    <img src="img/thumbs/3.png" alt="Rocky" class="icon">
+                    <span>3</span>
+                </a>
+                <div class="overlay" id="gambar3">
+                    <div class="bingkai">
+                        <a href="#gambar" class="close">X</a>
+                        <a href="#gambar1" class="prev"></a>
+                        <img src="img/full/3.png" alt="Rocky">
+                        <a href="#gambar5" class="next"></a>
+                        <a href="#gambar1" class="bullet">1</a>
+                        <a href="#gambar3" class="bullet">2</a>
+                        <a href="#gambar5" class="bullet">3</a>
+                        <a href="#gambar7" class="bullet">4</a>
+                        <a href="#gambar8" class="bullet">5</a>
+                        <a href="#gambar9" class="bullet">6</a>
+                        <a href="#gambar10" class="bullet">7</a>
+                        <a href="#gambar11" class="bullet">8</a>
+                        <a href="#gambar12" class="bullet">9</a>
+                        <a href="#gambar13" class="bullet">10</a>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <a href="#gambar5">
+                    <img src="img/thumbs/5.png" alt="Rocky" class="icon">
+                    <span>5</span>
+                </a>
+                <div class="overlay" id="gambar5">
+                    <div class="bingkai">
+                        <a href="#gambar" class="close">X</a>
+                        <a href="#gambar3" class="prev"></a>
+                        <img src="img/full/5.png" alt="Rocky">
+                        <a href="#gambar7" class="next"></a>
+                        <a href="#gambar1" class="bullet">1</a>
+                        <a href="#gambar3" class="bullet">2</a>
+                        <a href="#gambar5" class="bullet">3</a>
+                        <a href="#gambar7" class="bullet">4</a>
+                        <a href="#gambar8" class="bullet">5</a>
+                        <a href="#gambar9" class="bullet">6</a>
+                        <a href="#gambar10" class="bullet">7</a>
+                        <a href="#gambar11" class="bullet">8</a>
+                        <a href="#gambar12" class="bullet">9</a>
+                        <a href="#gambar13" class="bullet">10</a>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <a href="#gambar7">
+                    <img src="img/thumbs/7.png" alt="Rocky" class="icon">
+                    <span>7</span>
+                </a>
+                <div class="overlay" id="gambar7">
+                    <div class="bingkai">
+                        <a href="#gambar" class="close">X</a>
+                        <a href="#gambar5" class="prev"></a>
+                        <img src="img/full/7.png" alt="Rocky">
+                        <a href="#gambar8" class="next"></a>
+                        <a href="#gambar1" class="bullet">1</a>
+                        <a href="#gambar3" class="bullet">2</a>
+                        <a href="#gambar5" class="bullet">3</a>
+                        <a href="#gambar7" class="bullet">4</a>
+                        <a href="#gambar8" class="bullet">5</a>
+                        <a href="#gambar9" class="bullet">6</a>
+                        <a href="#gambar10" class="bullet">7</a>
+                        <a href="#gambar11" class="bullet">8</a>
+                        <a href="#gambar12" class="bullet">9</a>
+                        <a href="#gambar13" class="bullet">10</a>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <a href="#gambar8">
+                    <img src="img/thumbs/8.png" alt="Rocky" class="icon">
+                    <span>8</span>
+                </a>
+                <div class="overlay" id="gambar8">
+                    <div class="bingkai">
+                        <a href="#gambar" class="close">X</a>
+                        <a href="#gambar7" class="prev"></a>
+                        <img src="img/full/8.png" alt="Rocky">
+                        <a href="#gambar9" class="next"></a>
+                        <a href="#gambar1" class="bullet">1</a>
+                        <a href="#gambar3" class="bullet">2</a>
+                        <a href="#gambar5" class="bullet">3</a>
+                        <a href="#gambar7" class="bullet">4</a>
+                        <a href="#gambar8" class="bullet">5</a>
+                        <a href="#gambar9" class="bullet">6</a>
+                        <a href="#gambar10" class="bullet">7</a>
+                        <a href="#gambar11" class="bullet">8</a>
+                        <a href="#gambar12" class="bullet">9</a>
+                        <a href="#gambar13" class="bullet">10</a>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <a href="#gambar9">
+                    <img src="img/thumbs/9.png" alt="Rocky" class="icon">
+                    <span>9</span>
+                </a>
+                <div class="overlay" id="gambar9">
+                    <div class="bingkai">
+                        <a href="#gambar" class="close">X</a>
+                        <a href="#gambar8" class="prev"></a>
+                        <img src="img/full/9.png" alt="Rocky">
+                        <a href="#gambar10" class="next"></a>
+                        <a href="#gambar1" class="bullet">1</a>
+                        <a href="#gambar3" class="bullet">2</a>
+                        <a href="#gambar5" class="bullet">3</a>
+                        <a href="#gambar7" class="bullet">4</a>
+                        <a href="#gambar8" class="bullet">5</a>
+                        <a href="#gambar9" class="bullet">6</a>
+                        <a href="#gambar10" class="bullet">7</a>
+                        <a href="#gambar11" class="bullet">8</a>
+                        <a href="#gambar12" class="bullet">9</a>
+                        <a href="#gambar13" class="bullet">10</a>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <a href="#gambar10">
+                    <img src="img/thumbs/10.png" alt="Rocky" class="icon">
+                    <span>10</span>
+                </a>
+                <div class="overlay" id="gambar10">
+                    <div class="bingkai">
+                        <a href="#gambar" class="close">X</a>
+                        <a href="#gambar9" class="prev"></a>
+                        <img src="img/full/10.png" alt="Rocky">
+                        <a href="#gambar11" class="next"></a>
+                        <a href="#gambar1" class="bullet">1</a>
+                        <a href="#gambar3" class="bullet">2</a>
+                        <a href="#gambar5" class="bullet">3</a>
+                        <a href="#gambar7" class="bullet">4</a>
+                        <a href="#gambar8" class="bullet">5</a>
+                        <a href="#gambar9" class="bullet">6</a>
+                        <a href="#gambar10" class="bullet">7</a>
+                        <a href="#gambar11" class="bullet">8</a>
+                        <a href="#gambar12" class="bullet">9</a>
+                        <a href="#gambar13" class="bullet">10</a>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <a href="#gambar11">
+                    <img src="img/thumbs/11.png" alt="Rocky" class="icon">
+                    <span>11</span>
+                </a>
+                <div class="overlay" id="gambar11">
+                    <div class="bingkai">
+                        <a href="#gambar" class="close">X</a>
+                        <a href="#gambar10" class="prev"></a>
+                        <img src="img/full/11.png" alt="Rocky">
+                        <a href="#gambar12" class="next"></a>
+                        <a href="#gambar1" class="bullet">1</a>
+                        <a href="#gambar3" class="bullet">2</a>
+                        <a href="#gambar5" class="bullet">3</a>
+                        <a href="#gambar7" class="bullet">4</a>
+                        <a href="#gambar8" class="bullet">5</a>
+                        <a href="#gambar9" class="bullet">6</a>
+                        <a href="#gambar10" class="bullet">7</a>
+                        <a href="#gambar11" class="bullet">8</a>
+                        <a href="#gambar12" class="bullet">9</a>
+                        <a href="#gambar13" class="bullet">10</a>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <a href="#gambar12">
+                    <img src="img/thumbs/12.png" alt="Rocky" class="icon">
+                    <span>12</span>
+                </a>
+                <div class="overlay" id="gambar12">
+                    <div class="bingkai">
+                        <a href="#gambar" class="close">X</a>
+                        <a href="#gambar11" class="prev"></a>
+                        <img src="img/full/12.png" alt="Rocky">
+                        <a href="#gambar13" class="next"></a>
+                        <a href="#gambar1" class="bullet">1</a>
+                        <a href="#gambar3" class="bullet">2</a>
+                        <a href="#gambar5" class="bullet">3</a>
+                        <a href="#gambar7" class="bullet">4</a>
+                        <a href="#gambar8" class="bullet">5</a>
+                        <a href="#gambar9" class="bullet">6</a>
+                        <a href="#gambar10" class="bullet">7</a>
+                        <a href="#gambar11" class="bullet">8</a>
+                        <a href="#gambar12" class="bullet">9</a>
+                        <a href="#gambar13" class="bullet">10</a>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <a href="#gambar13">
+                    <img src="img/thumbs/13.png" alt="Rocky" class="icon">
+                    <span>13</span>
+                </a>
+                <div class="overlay" id="gambar13">
+                    <div class="bingkai">
+                        <a href="#gambar" class="close">X</a>
+                        <a href="#gambar12" class="prev"></a>
+                        <img src="img/full/13.png" alt="Rocky">
+                        <a href="#gambar1" class="next"></a>
+                        <a href="#gambar1" class="bullet">1</a>
+                        <a href="#gambar3" class="bullet">2</a>
+                        <a href="#gambar5" class="bullet">3</a>
+                        <a href="#gambar7" class="bullet">4</a>
+                        <a href="#gambar8" class="bullet">5</a>
+                        <a href="#gambar9" class="bullet">6</a>
+                        <a href="#gambar10" class="bullet">7</a>
+                        <a href="#gambar11" class="bullet">8</a>
+                        <a href="#gambar12" class="bullet">9</a>
+                        <a href="#gambar13" class="bullet">10</a>
+                    </div>
+                </div>
+            </li>
+            <div class="clear"></div>
+
+        </ul>
+    </div>
+</body>
+
+</html>
